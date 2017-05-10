b9bf79e772fd1a937f0309e7c6db7a8e0539dc22
Luqqk <mikolajczak.luq@gmail.com>
2017-05-10 20:01:52 +0200

    Historical rates /{date} route handler

--- goexrates.go
+++ goexrates.go
@@ -5,6 +5,7 @@ import (
 	"goexrates/models"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -16,6 +17,7 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/latest", Latest)
+	router.HandleFunc("/{date}", Historical)
 	loggedRouter := handlers.LoggingHandler(os.Stdout, router)
 
 	http.ListenAndServe(":3000", loggedRouter)
@@ -27,6 +29,34 @@ func Latest(w http.ResponseWriter, r *http.Request) {
 	symbolsParam := r.URL.Query().Get("symbols")
 
 	currencies, err := models.LatestRates(baseParam, symbolsParam)
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	response, err := json.Marshal(currencies)
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+}
+
+//Historical : /{date} route handler
+func Historical(w http.ResponseWriter, r *http.Request) {
+	baseParam := r.URL.Query().Get("base")
+	symbolsParam := r.URL.Query().Get("symbols")
+	vars := mux.Vars(r)
+
+	_, err := time.Parse("2006-01-02", vars["date"])
+	if err != nil {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+
+	currencies, err := models.HistoricalRates(baseParam, symbolsParam, vars["date"])
 	if err != nil {
 		http.Error(w, http.StatusText(500), 500)
 		return
--- models/currencies.go
+++ models/currencies.go
@@ -102,3 +102,96 @@ func LatestRates(baseParam, symbolsParam string) (*Currencies, error) {
 
 	return currencies, nil
 }
+
+// HistoricalRates query db for most updated results for each currency
+// supports different queries based on request parameters (base, symbols)
+func HistoricalRates(baseParam, symbolsParam, dateParam string) (*Currencies, error) {
+	base := strings.ToUpper(baseParam)
+	symbols := strings.ToUpper(symbolsParam)
+	if base == "EUR" {
+		base = ""
+	}
+
+	var rows *sql.Rows
+	var err error
+
+	switch {
+	case base != "" && symbols != "":
+		if strings.Contains(symbols, "EUR") {
+			rows, err = db.Query(`SELECT currency, 
+			rate / (SELECT rate FROM rates 
+			WHERE ratedate = (SELECT ratedate FROM rates WHERE ratedate <= $3 LIMIT 1) 
+			AND currency = $1) AS rate FROM rates WHERE currency !=$1 
+			AND currency = ANY(string_to_array($2, ',')) 
+			AND ratedate = (SELECT ratedate FROM rates WHERE ratedate <= $3 LIMIT 1) 
+			UNION ALL SELECT 'EUR', 1 / (SELECT rate FROM rates 
+			WHERE ratedate = (SELECT ratedate FROM rates WHERE ratedate <= $3 LIMIT 1) 
+			AND currency = $1)`, base, symbols, dateParam)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			rows, err = db.Query(`SELECT currency, 
+			rate / (SELECT rate FROM rates 
+			WHERE ratedate = (SELECT ratedate FROM rates WHERE ratedate <= $3 LIMIT 1) 
+			AND currency = $1) AS rate FROM rates WHERE currency !=$1 
+			AND currency = ANY(string_to_array($2, ',')) 
+			AND ratedate = (SELECT max(ratedate) FROM rates)`, base, symbols, dateParam)
+			if err != nil {
+				return nil, err
+			}
+		}
+	case base != "" && symbols == "":
+		rows, err = db.Query(`SELECT currency, 
+		rate / (SELECT rate FROM rates WHERE ratedate = (SELECT ratedate FROM rates WHERE ratedate <= $2 LIMIT 1) 
+		AND currency = $1) AS rate FROM rates WHERE currency !=$1 
+		AND ratedate = (SELECT ratedate FROM rates WHERE ratedate <= $2 LIMIT 1) 
+		UNION ALL SELECT 'EUR', 1 / (SELECT rate FROM rates 
+		WHERE ratedate = (SELECT ratedate FROM rates WHERE ratedate <= $2 LIMIT 1)`, base, dateParam)
+		if err != nil {
+			return nil, err
+		}
+	case base == "" && symbols != "":
+		rows, err = db.Query(`SELECT currency, rate 
+		FROM rates WHERE currency = ANY(string_to_array($1, ',')) 
+		AND ratedate = (SELECT ratedate FROM rates WHERE ratedate <= $2 LIMIT 1)`, symbols, dateParam)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		rows, err = db.Query(`SELECT currency, rate FROM rates 
+		WHERE ratedate = (SELECT ratedate FROM rates WHERE ratedate <= $1 LIMIT 1)`,
+			dateParam)
+		if err != nil {
+			return nil, err
+		}
+	}
+	defer rows.Close()
+
+	currencies := &Currencies{Rates: make(map[string]float32)}
+	for rows.Next() {
+		var (
+			currency string
+			rate     float32
+		)
+		if err := rows.Scan(&currency, &rate); err != nil {
+			return nil, err
+		}
+		currencies.Rates[currency] = rate
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	var date string
+	err = db.QueryRow("SELECT ratedate FROM rates WHERE ratedate <= $1 LIMIT 1", date).Scan(&date)
+	currencies.Date = date
+
+	if base != "" {
+		currencies.Base = base
+	} else {
+		currencies.Base = "EUR"
+	}
+
+	return currencies, nil
+}
