a23ff5d6725479e055bbd3c3be5143bd07c3c021
Luqqk <mikolajczak.luq@gmail.com>
2021-08-08 22:22:15 +0200

    Adjust database tables

--- migrations/000001_init_database.up.sql
+++ migrations/000001_init_database.up.sql
@@ -1,7 +1,5 @@
 CREATE TABLE IF NOT EXISTS currencies (
     id serial PRIMARY KEY,
-    entity text NOT NULL,
-    label text NOT NULL,
     code text NOT NULL,
     updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
@@ -10,7 +8,7 @@ CREATE TABLE IF NOT EXISTS currencies (
 CREATE TABLE IF NOT EXISTS rates (
     id serial PRIMARY KEY,
     currency_id int NOT NULL REFERENCES currencies(id) ON DELETE CASCADE,
-    base text NOT NULL,
     rate numeric NOT NULL,
+    updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
 );
