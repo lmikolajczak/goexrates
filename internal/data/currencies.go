d20b993af4825dd58e6b7463ca76073977b8a0bf
Luqqk <mikolajczak.luq@gmail.com>
2021-08-10 23:38:31 +0200

    Insert(currency *Currency) into db

--- internal/data/currencies.go
+++ internal/data/currencies.go
@@ -17,9 +17,21 @@ type CurrencyModel struct {
 	DB *sql.DB
 }
 
-// Placeholder for inserting new currency into the database
+// Insert for inserting new currency into the database
 func (c *CurrencyModel) Insert(currency *Currency) error {
-	return nil
+	// Define the SQL query for inserting a new record in the currencies table and
+	// returning the system-generated data.
+	query := `
+		INSERT INTO currencies (code) VALUES ($1)
+		RETURNING id, updated_at, created_at`
+	// Use the QueryRow() method to execute the SQL query on our connection pool
+	// passing in the currency code as a parameter and scanning the system-generated
+	// id, updated_at and created_at values into the currency struct.
+	return c.DB.QueryRow(
+		query, currency.Code,
+	).Scan(
+		&currency.Id, &currency.UpdatedAt, &currency.CreatedAt,
+	)
 }
 
 // Placeholder for fetching currency from the database
