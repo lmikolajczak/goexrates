a859cd5394341d075920050820962edefc5258a4
Luqqk <mikolajczak.luq@gmail.com>
2021-08-12 22:22:30 +0200

    Insert(rate *Rate) into db

--- internal/data/rates.go
+++ internal/data/rates.go
@@ -18,9 +18,21 @@ type RateModel struct {
 	DB *sql.DB
 }
 
-// Placeholder for inserting new rate into the database
+// Insert for inserting new rate into the database
 func (r *RateModel) Insert(rate *Rate) error {
-	return nil
+	// Define the SQL query for inserting a new record in the rates table and
+	// returning the system-generated data.
+	query := `
+		INSERT INTO rates (currency_id, rate) VALUES ($1, $2)
+		RETURNING id, updated_at, created_at`
+	// Use the QueryRow() method to execute the SQL query on our connection pool
+	// passing in the rate code and rate as a parameters and scanning the system-
+	// generated id, updated_at and created_at values into the rate struct.
+	return r.DB.QueryRow(
+		query, rate.CurrencyId, rate.Rate,
+	).Scan(
+		&rate.Id, &rate.UpdatedAt, &rate.CreatedAt,
+	)
 }
 
 // Placeholder for fetching rate from the database
